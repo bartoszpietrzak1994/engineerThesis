af0e93f9e6188ffd8ba9e494cae38c0db06a69c3
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-05-02 23:07:30 +0200

    .fxml file connected with java fxml controller

--- src/main/controller/MainWindowController.java
+++ src/main/controller/MainWindowController.java
@@ -4,7 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import model.album.AlbumSortingCriterias;
 import org.springframework.stereotype.Component;
 
@@ -20,6 +23,30 @@ final public class MainWindowController implements Initializable
     @FXML
     private ComboBox<String> sortBy;
 
+    @FXML
+    private TextField userName;
+
+    @FXML
+    private Button addAlbum;
+
+    @FXML
+    private Button rate;
+
+    @FXML
+    private Button details;
+
+    @FXML
+    private Button getRecommendations;
+
+    @FXML
+    private Button sort;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private ImageView albumCoverPreview;
+
     @Override
     public void initialize(URL location, ResourceBundle resources)
     {
@@ -27,5 +54,41 @@ final public class MainWindowController implements Initializable
                 .collect(Collectors.toList());
         ObservableList<String> ioOperations = FXCollections.observableList(stringAlbumSortingCriterias);
         sortBy.setItems(ioOperations);
+    }
+
+    @FXML
+    public void onAddAlbumButtonClicked()
+    {
+
+    }
+
+    @FXML
+    public void onRateButtonClicked()
+    {
+
+    }
+
+    @FXML
+    public void onDetailsButtonClicked()
+    {
+
+    }
+
+    @FXML
+    public void onGetRecommendationsButtonClicked()
+    {
+
+    }
+
+    @FXML
+    public void onSortButtonClicked()
+    {
+
+    }
+
+    @FXML
+    public void onLogoutButtonClicked()
+    {
+
     }
 }
