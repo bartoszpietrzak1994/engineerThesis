c19be3cddfe0892094a4a9136cd2bd161bac9f26
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-06-02 13:14:26 +0200

    FindUserByUsername tests

---
+++ src/test/java/unit/authentication/AuthenticationProviderItFindsUserByUsername.java
@@ -0,0 +1,26 @@
+package unit.authentication;
+
+import model.user.User;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class AuthenticationProviderItFindsUserByUsername extends BaseAuthenticationProviderTest
+{
+    @Test
+    public void itReturnsUserEntity()
+    {
+        // GIVEN
+        Mockito.when(userRepository.findOneByUserName(USERNAME)).thenReturn(new User());
+
+        // WHEN
+        User userByUsername = authenticationProvider.findUserByUsername(USERNAME);
+
+        // THEN
+        assertThat(userByUsername).isInstanceOf(User.class);
+        assertThat(userByUsername).hasFieldOrProperty("userId");
+        assertThat(userByUsername).hasFieldOrProperty("userName");
+        assertThat(userByUsername).hasFieldOrProperty("password");
+    }
+}
